82b22fd5dce801bf5b47a229dcc61fcdd4f07aab
Andy Hawkins <andy@gently.org.uk>
2016-02-09 21:43:02 +0000

    Initial packaging work

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="pysmugmugsync",
+
+    # Version number (initial):
+    version="0.0.1",
+
+    # Application author details:
+    author="Andy Hawkins",
+    author_email="andy@gently.org.uk",
+
+    # Packages
+    packages=["smugmugsyncutils"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://github.com/adhawkins/pysmugmugsync",
+
+    #
+    license="LICENSE.txt",
+    description="A Python based synchronisation client for SmugMug.",
+
+    long_description=open("README.md").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+    ],
+)
