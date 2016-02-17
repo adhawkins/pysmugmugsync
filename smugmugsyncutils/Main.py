22d8ed8d484eced55a0adbf28f4cd94facc2f641
Andy Hawkins <andy@gently.org.uk>
2016-02-17 22:33:43 +0000

    Start creating top level albums and folders

--- smugmugsyncutils/Main.py
+++ smugmugsyncutils/Main.py
@@ -1,13 +1,20 @@
 from argparse import ArgumentParser
 from Config import Config
-from smugmugv2py import SmugMugConnection
+from smugmugv2py import Connection, User, Node, Album, SmugMugv2Utils
 from sys import stdout, stdin
 from os import linesep
 from json import loads
+from SmugMugLocalAlbum import SmugMugLocalAlbum
+from pprint import pprint
 
-api_key = "Bwlfnuv1WMNUkcPynYuElBDBy2e8k7Am"
-api_secret = "e808da94417c928654353b1199afdebf"
+api_key = "LXTk156AmDT0IhjLuDetBUwP9nWKCppg"
+api_secret = "be735fbb3b44698d72506b29e21a434c"
 
+def print_album(album):
+	print str(album)
+	for child in album.children:
+		print_album(child)
+		
 def main():
 	parser = ArgumentParser()
 	parser.add_argument("site", help="the site to upload to")
@@ -19,10 +26,10 @@ def main():
 
 	config = Config(args.site)
 
-	connection = SmugMugConnection(api_key, api_secret)
+	connection = Connection(api_key, api_secret)
 
 	if args.reauth or not config.token or not config.secret:
-		auth_url = connection.get_auth_url()
+		auth_url = connection.get_auth_url(access="Full", permissions="Modify")
 
 		print "Visit the following URL and retrieve a verification code:%s%s" % (linesep, auth_url)
 
@@ -37,7 +44,17 @@ def main():
 		config.write(args.site)
 
 	connection.authorise_connection(config.token, config.secret)
+	user = User(SmugMugv2Utils.get_authorized_user(connection))
+	print "User: " + user.nickname + " (" + user.name + ")"
+	root_node = Node(SmugMugv2Utils.get_node(connection,user.node))
+	
+	root = SmugMugLocalAlbum(args.path)
 
-	json = connection.test_connection()
-	result = loads(json)
-	print result["Response"]["User"]["Name"]
+	for child in root.children:
+		if child.items:
+			node=Node(root_node.create_child_album(connection, child.name, child.album_name, 'Public', child.name))
+			pprint(node.album)
+			album=Album(SmugMugv2Utils.get_album(connection, node.album))
+		else:
+			node=Node(root_node.create_child_folder(connection, child.name, child.album_name, 'Public'))
+			
