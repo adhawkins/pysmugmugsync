697f0c8b30521528ae4e01fbdbeea18f145bf356
Andy Hawkins <andy@gently.org.uk>
2016-03-14 23:02:43 +0000

    Fix incorrect import

--- pysmugmugsync/__init__.py
+++ pysmugmugsync/__init__.py
@@ -1,5 +1,4 @@
 from SmugMugLocalAlbum import SmugMugLocalAlbum
-from SmugMugRemoteAlbum import SmugMugRemoteAlbum
 from Main import main
 
-__all__ = ["SmugMugLocalAlbum", "SmugMugRemoteAlbum", "main"]
+__all__ = ["SmugMugLocalAlbum", "main"]
